6f225c858815cb3f8bf3b3fd0fbbde806fed156b
Indradeep Mandal <screechin_03@Indradeeps-MacBook-Air.local>
2025-01-23 20:19:27 +0530

    Final changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://community-533m.vercel.app/api",
     withCredentials: true,
 })
