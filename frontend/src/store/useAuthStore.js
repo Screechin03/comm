7cdd78eadeb0d2db56606bf1cd93e652b7751100
Indradeep Mandal <screechin_03@Indradeeps-MacBook-Air.local>
2025-01-24 13:50:05 +0530

    Describe the changes here

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,8 +2,6 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-
-
 export const useAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
