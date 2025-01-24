d1a9a45a5c8b3fad15d47f220e3cbb2997e8788b
Indradeep Mandal <indradeepmandal18@gmail.com>
2025-01-24 10:56:50 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 const cors = require('cors');
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://comm-2.onrender.com",
     credentials: true,
 }
 ))
